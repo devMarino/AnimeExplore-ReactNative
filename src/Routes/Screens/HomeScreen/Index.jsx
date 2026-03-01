c078005ec8a43b1e96deb763af0eb565586ced57
devMarino <marcelomcb24@gmail.com>
2026-03-01 16:23:33 -0300

    Aplicando get TopAnimes + BuscaTopAnimes com useEffect + efeito de loading

--- src/Routes/Screens/HomeScreen/Index.jsx
+++ src/Routes/Screens/HomeScreen/Index.jsx
@@ -1,8 +1,42 @@
-import { Text } from "react-native";
-
+import { ActivityIndicator, Text, View } from "react-native";
+import Api from "../../../services/Api";
+import { useEffect, useState } from "react";
 export default function HomeScreen({navigation}){
-    return(
-        <Text>Home</Text>
-    )
 
+    const [pageTopAnime, setPageTopAnime] = useState(1)
+    const [animes, setAnimes] = useState([])
+
+    const [error, setError] = useState(false)
+    const [loading,setLoading] = useState(false)
+
+    const BuscarTopAnimes = async () => {
+        setLoading(true)
+        try{
+            const response = await Api.get(`/top/anime?page=${pageTopAnime}`)
+            // salvando lista de Top animes
+            setAnimes(response.data.data)
+            }  
+        catch(error){
+                setError(true)
+        } finally {
+                setLoading(false)
+            }
+        }
+
+    useEffect(() => {
+    // instanciando a função para buscar animes
+    BuscarTopAnimes()
+    },[])
+    // ativa style enquanto carrega animes
+    if (loading) return <ActivityIndicator size='large' color="#ffffff"/>
+
+    if (error) return <Text>Erro ao carregar animes</Text>
+
+     return(
+        <View>
+            <Text>Total de animes: {animes.length}</Text>
+        </View>
+    )
+    
 }
+
